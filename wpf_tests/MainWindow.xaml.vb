ded04c5a1975b849d006fc455c6e2b687a77ec5c
5gMurilo <cardoso.murilo2002@gmail.com>
2022-06-22 08:09:55 -0300

    :lipstick: ui initialized

---
+++ wpf_tests/MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Imports MySql.Data.MySqlClient
+Class MainWindow
+    Dim conn As MySqlConnection
+
+    Private Sub txt_username_OnFocus() Handles txt_username.GotFocus
+        MsgBox("Você focou a textBox", 0, "Oba!")
+    End Sub
+End Class
